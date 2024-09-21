281957daf24a3e3d74e2e17acced07cfdda0d5c2
Kacian417 <knicho4492@gmail.com>
2024-09-21 00:19:43 +0000

    created domain names

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  /*  let colors = ["red", "blue", "pink", "green", "orange", "yellow", "purple"];
+  let animals = ["dog", "cat", "piggy", "cow", "horse", "gator", "elephant"];
+
+  for (let colorIndex = 0; colorIndex < colors.length; colorIndex++) {
+    for (let animalsIndex = 0; animalsIndex < animals.length; animalsIndex++) {
+      console.log(colors[colorIndex] + animals[animalsIndex]);
+    }
+  }
+*/
+  let pronoun = ["the", "our", "his"];
+  let adj = ["great", "big", "smallish"];
+  let noun = ["jogger", "racoon", "spaceman"];
+  let extensions = [".com", ".us", ".net"];
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        for (
+          let extensionsIndex = 0;
+          extensionsIndex < extensions.length;
+          extensionsIndex++
+        ) {
+          console.log(
+            pronoun[pronounIndex] +
+              adj[adjIndex] +
+              noun[nounIndex] +
+              extensions[extensionsIndex]
+          );
+        }
+      }
+    }
+  }
 };
